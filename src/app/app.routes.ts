e9b1c8cda6402e093813162d194f357b41380b0f
Diekkgo <da.gomezs12@uniandes.edu.co>
2026-04-21 17:11:53 -0500

    feat: Módulo de inicio implementado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { LoginComponent } from './modules/auth/pages/login/login.component';
 import { ListUserComponent } from './modules/user/pages/list-user/list-user.component';
 import { CreateUserComponent } from './modules/user/pages/create-user/create-user.component';
 import { AssociateRoleComponent } from './modules/user/pages/associate-role/associate-role.component';
+import { HomeComponent } from './modules/home/pages/home/home.component';
 
 export const routes: Routes = [
     {
         path: 'auth',
         component: LoginComponent
+    },
+    {
+        path: 'home',
+        component: HomeComponent
     },
     {
         path: 'users',
