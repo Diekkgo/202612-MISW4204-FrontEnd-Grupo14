f5aebe30708598de6507321caab214fefb103620
fjaramillouniandes <fm.jaramillo@uniandes.edu.co>
2026-04-26 10:52:39 -0500

    role restrictions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,103 +1,155 @@
 import { Routes } from '@angular/router';
+
 import { LoginComponent } from './modules/auth/pages/login/login.component';
+import { HomeComponent } from './modules/home/pages/home/home.component';
+
 import { ListUserComponent } from './modules/user/pages/list-user/list-user.component';
+import { CreateUserComponent } from './modules/user/pages/create-user/create-user.component';
+import { AssociateRoleComponent } from './modules/user/pages/associate-role/associate-role.component';
+
 import { ListAssignmentsComponent } from './modules/assignments/pages/list-assignments/list-assignments.component';
 import { CreateAssignmentComponent } from './modules/assignments/pages/create-assignment/create-assignment.component';
-import { EditTaskComponent } from './modules/tasks/pages/edit-task/edit-task.component';
-import { TaskDetailComponent } from './modules/tasks/pages/task-detail/task-detail.component';
-import { CreateTaskComponent } from './modules/tasks/pages/create-task/create-task.component';
+
 import { ListTasksComponent } from './modules/tasks/pages/list-tasks/list-tasks.component';
-import { HomeComponent } from './modules/home/pages/home/home.component';
+import { CreateTaskComponent } from './modules/tasks/pages/create-task/create-task.component';
+import { TaskDetailComponent } from './modules/tasks/pages/task-detail/task-detail.component';
+import { EditTaskComponent } from './modules/tasks/pages/edit-task/edit-task.component';
+
 import { ListReportsComponent } from './modules/reports/pages/list-reports/list-reports.component';
 import { GenerateReportsComponent } from './modules/reports/pages/generate-reports/generate-reports.component';
+
 import { ListCourseComponent } from './modules/course/pages/list-course/list-course.component';
 import { CreateCourseComponent } from './modules/course/pages/create-course/create-course.component';
 import { CourseDetailsComponent } from './modules/course/pages/course-details/course-details.component';
-import { AssociateRoleComponent } from './modules/user/pages/associate-role/associate-role.component';
-import { CreateUserComponent } from './modules/user/pages/create-user/create-user.component';
+
 import { ProfessorComponent } from './modules/professor/pages/professor.component';
 import { OperationalComponent } from './modules/operational/pages/operational.component';
 
+import { authGuard } from './core/guards/auth.guard';
+import { guestGuard } from './core/guards/guest.guard';
+import { roleGuard } from './core/guards/role.guard';
+
 export const routes: Routes = [
-    {
-        path: 'auth',
-        component: LoginComponent
-    },
-    {
-        path: 'home',
-        component: HomeComponent
-    },
-    {
-        path: 'users',
-        component: ListUserComponent
-    },
-    {
-        path: 'users/create',
-        component: CreateUserComponent
-    },
-    {
-        path: 'users/:id/roles',
-        component: AssociateRoleComponent
-    },
-    {
-        path: 'assignments',
-        component: ListAssignmentsComponent
-    },
-    {
-        path: 'assignments/create',
-        component: CreateAssignmentComponent
-    },
-    {
-        path:'tasks',
-        component:ListTasksComponent
-    },
-    {
-        path:'tasks/create',
-        component:CreateTaskComponent
-    },
-    {
-        path:'tasks/:id',
-        component:TaskDetailComponent
-    },
-    {
-        path:'tasks/:id/edit',
-        component:EditTaskComponent
-    },
-    {
-        path: 'reports',
-        component: ListReportsComponent
-    },
-    {
-        path: 'reports/generate',
-        component: GenerateReportsComponent
-    },
-    {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'auth'
-    },
-    {
-        path: 'courses',
-        component: ListCourseComponent
-    },
-    {
-        path: 'courses/create',
-        component: CreateCourseComponent
-    },
-    {
-        path: 'courses/details/:id',
-        component: CourseDetailsComponent
-    },
-    {
-        path: 'professor',
-        component: ProfessorComponent
-    },
-    {
-        path: 'student-monitor',
-        component: OperationalComponent
-    },
-    {
-        path: '**',
-        redirectTo: 'auth'
-    }
+  {
+    path: 'auth',
+    component: LoginComponent,
+    canActivate: [guestGuard]
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [authGuard]
+  },
+
+  {
+    path: 'users',
+    component: ListUserComponent,
+    canActivate: [roleGuard],
+    data: { roles: ['ADMIN'] }
+  },
+  {
+    path: 'users/create',
+    component: CreateUserComponent,
+    canActivate: [roleGuard],
+    data: { roles: ['ADMIN'] }
+  },
+  {
+    path: 'users/:id/roles',
+    component: AssociateRoleComponent,
+    canActivate: [roleGuard],
+    data: { roles: ['ADMIN'] }
+  },
+
+  {
+    path: 'assignments',
+    component: ListAssignmentsComponent,
+    canActivate: [roleGuard],
+    data: { roles: ['PROFESOR', 'ADMIN'] }
+  },
+  {
+    path: 'assignments/create',
+    component: CreateAssignmentComponent,
+    canActivate: [roleGuard],
+    data: { roles: ['PROFESOR', 'ADMIN'] }
+  },
+
+  {
+    path: 'tasks',
+    component: ListTasksComponent,
+    canActivate: [roleGuard],
+    data: { roles: ['ESTUDIANTE', 'PROFESOR', 'ADMIN'] }
+  },
+  {
+    path: 'tasks/create',
+    component: CreateTaskComponent,
+    canActivate: [roleGuard],
+    data: { roles: ['ESTUDIANTE'] }
+  },
+  {
+    path: 'tasks/:id/edit',
+    component: EditTaskComponent,
+    canActivate: [roleGuard],
+    data: { roles: ['ESTUDIANTE'] }
+  },
+  {
+    path: 'tasks/:id',
+    component: TaskDetailComponent,
+    canActivate: [roleGuard],
+    data: { roles: ['ESTUDIANTE', 'PROFESOR', 'ADMIN'] }
+  },
+
+  {
+    path: 'reports',
+    component: ListReportsComponent,
+    canActivate: [roleGuard],
+    data: { roles: ['PROFESOR', 'ADMIN'] }
+  },
+  {
+    path: 'reports/generate',
+    component: GenerateReportsComponent,
+    canActivate: [roleGuard],
+    data: { roles: ['PROFESOR', 'ADMIN'] }
+  },
+
+  {
+    path: 'courses',
+    component: ListCourseComponent,
+    canActivate: [roleGuard],
+    data: { roles: ['PROFESOR', 'ADMIN'] }
+  },
+  {
+    path: 'courses/create',
+    component: CreateCourseComponent,
+    canActivate: [roleGuard],
+    data: { roles: ['PROFESOR', 'ADMIN'] }
+  },
+  {
+    path: 'courses/details/:id',
+    component: CourseDetailsComponent,
+    canActivate: [roleGuard],
+    data: { roles: ['PROFESOR', 'ADMIN'] }
+  },
+
+  {
+    path: 'professor',
+    component: ProfessorComponent,
+    canActivate: [roleGuard],
+    data: { roles: ['PROFESOR', 'ADMIN'] }
+  },
+  {
+    path: 'student-monitor',
+    component: OperationalComponent,
+    canActivate: [roleGuard],
+    data: { roles: ['ESTUDIANTE', 'ADMIN'] }
+  },
+
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'auth'
+  },
+  {
+    path: '**',
+    redirectTo: 'auth'
+  }
 ];
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,29 +1,71 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+
 import { TokenService } from '../../../core/services/token.service';
 import { NotificationComponent } from '../../../modules/notification/pages/notification.component';
 import { ListWeeksComponent } from '../../../modules/weeks/pages/list-weeks/list-weeks.component';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [RouterModule, CommonModule, NotificationComponent, ListWeeksComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent implements OnInit {
-  userRole: string[] = [];
-
-  isAdmin = false;
-  isProfessor = false;
   private readonly tokenService = inject(TokenService);
+
+  userRoles: string[] = [];
 
   ngOnInit(): void {
     this.tokenService.roles$.subscribe((roles) => {
-      this.userRole = roles;
-      this.isAdmin = roles.includes('ADMIN');
-      this.isProfessor = roles.includes('PROFESOR');
+      this.userRoles = roles;
     });
+  }
+
+  get isLoggedIn(): boolean {
+    return this.tokenService.hasToken();
+  }
+
+  get isAdmin(): boolean {
+    return this.tokenService.isAdmin();
+  }
+
+  get isProfessor(): boolean {
+    return this.tokenService.isProfessor();
+  }
+
+  get isStudent(): boolean {
+    return this.tokenService.isStudent();
+  }
+
+  get canSeeUsers(): boolean {
+    return this.isAdmin;
+  }
+
+  get canSeeAssignments(): boolean {
+    return this.isProfessor || this.isAdmin;
+  }
+
+  get canSeeTasks(): boolean {
+    return this.isStudent || this.isProfessor || this.isAdmin;
+  }
+
+  get canSeeReports(): boolean {
+    return this.isProfessor || this.isAdmin;
+  }
+
+  get canSeeCourses(): boolean {
+    return this.isProfessor || this.isAdmin;
+  }
+
+  get canSeeProfessorView(): boolean {
+    return this.isProfessor || this.isAdmin;
+  }
+
+  get canSeeStudentView(): boolean {
+    return this.isStudent || this.isAdmin;
   }
 
   protected logout(): void {
